ba8578aa8a03c7ef12ce8fd63dcbf2004ddd421b
Your Name <3ndetc>
2023-03-24 19:51:41 +0400

    Initial commit

---
+++ PH_Cat.py
@@ -0,0 +1,11 @@
+class Cat:
+    def __init__(self,name,gender,age):
+        self.name=name
+        self.gender=gender
+        self.age=age
+    def name(self):
+        return cat.name
+    def gender(self):
+        return cat.gender
+    def age(self):
+        return cat.age
